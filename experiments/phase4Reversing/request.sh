fe8de1393c48eab838f5193c82935f7241fffb8c
bänz <benz.schenk@brokkoli.be>
2021-12-23 14:12:13 +0100

    with these changes i'm now getting peppol profile errors, so i think this is going in the right direction.

--- experiments/phase4Reversing/request.sh
+++ experiments/phase4Reversing/request.sh
@@ -1,1 +1,1 @@
-curl -X POST -H 'Content-Type: application/xml' http://localhost:8080/as4 -d "@request.xml" > response.xml && xmllint --format response.xml > return.xml && rm response.xml
+curl -X POST -H 'Content-Type: application/xml' http://localhost:8080/as4 -d "@request.xml" > response.xml #&& xmllint --format response.xml > return.xml && rm response.xml
