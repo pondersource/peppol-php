9aee43794634cb4dee8b435621b1c7cd54fc1dcd
ishifoev <alex.malikov94@gmail.com>
2021-12-24 15:38:53 +0500

    Add Unit Code we have more but I did some list

---
+++ experiments/invoice-ubl/src/UnitCode.php
@@ -0,0 +1,25 @@
+<?php
+
+class UnitCode
+{
+    const UNIT = 'C62';
+    const PIECE = 'H87';
+    const GROUP = 10;
+    const ARE = 'ARE';
+    const HECTARE = 'HAR';
+    const OUTLIT = 11;
+    const SQUARE_METRE = 'MTK';
+    const SQUARE_KILOMETRE = 'KMK';
+    const SQUARE_FOOT = 'FTK';
+    const SQUARE_YARD = 'YDK';
+    const SQUARE_MILE = 'MIK';
+    const RATION = 13;
+    const LITRE = 'LTR';
+    const SHOT = 14;
+    const SECOND = 'SEC';
+    const MINUTE = 'MIN';
+    const HOUR = 'HUR';
+    const DAY = 'DAY';
+    const MONTH = 'MON';
+    const YEAR = 'ANN';
+}
