546eae9e7b3ba2527b11fe34708ea5c521b6e27a
ishifoev <alex.malikov94@gmail.com>
2021-12-27 11:06:49 +0500

    Finish Party and start LegalEntiy

--- experiments/invoice-ubl/src/Party.php
+++ experiments/invoice-ubl/src/Party.php
@@ -25,4 +25,94 @@ class Party {
         $this->endpointID = $endpointID;
         return $this;
     }
+
+    /**
+     * Seller identifier or bank assigned creditor identifier
+     */
+    public function setPartyIdentification(?string $partyIdentification): Party {
+        $this->partyIdentification = $partyIdentification;
+        return $this;
+    }
+
+    /**
+     * get Identification Party
+     */
+    public function getPartyIdentification(): ?string {
+        return $this->partyIdentification;
+    }
+
+    /**
+     * Seller trading name
+     */
+    public function getPartyName(): ?string {
+        return $this->partyName;
+    }
+
+    /**
+     * Set party Name
+     */
+    public function setPartyName(?string $partyName): Party {
+        $this->partyName = $partyName;
+        return $this;
+    }
+
+    /**
+     * return Postal Address
+     */
+    public function getPostalAddress(): ?PostalAddress {
+        return $this->postalAddress;
+    }
+
+    /**
+     * set postal address
+     */
+    public function setPostalAddress(?PostalAddress $postalAddress): Party {
+        $this->postalAddress = $postalAddress;
+        return $this;
+    }
+
+    /**
+     * return Address location
+     */
+    public function getPhysicalLocation(): ?PostalAddress {
+        return $this->physicalLocation;
+    }
+
+    /**
+     * Set Physical Location
+     */
+    public function setPhysicalLocation(?PostalAddress $physicalLocation): Party {
+        $this->physicalLocation = $physicalLocation;
+        return $this;
+    }
+
+    /**
+     * get Contact
+     */
+    public function getContact(): ?Contact {
+        return $this->contact;
+    }
+
+    /**
+     * Set Contact
+     */
+    public function setContact(?Contact $contact): Party {
+        $this->contact = $contact;
+        return $this;
+    }
+
+    /**
+     * Get Legal Entity
+     */
+    public function getLegalEntity(): ?LegalEntity {
+        return $this->legalEntity;
+    }
+
+    /**
+     * Set Legal Entity
+     */
+    public function setLegalEntity(?LegalEntity $legalEntity): Party {
+        $this->legalEntity = $legalEntity;
+        return $this;
+    }
 }
