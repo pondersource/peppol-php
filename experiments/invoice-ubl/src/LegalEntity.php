a1d2f21657b7bc20eb2faf067a440c61d7376b71
ishifoev <alex.malikov94@gmail.com>
2021-12-27 11:23:00 +0500

    Update Legal entity class

--- experiments/invoice-ubl/src/LegalEntity.php
+++ experiments/invoice-ubl/src/LegalEntity.php
@@ -1,5 +1,55 @@
 <?php
 
 class LegalEntity {
-    
+    private $registrationName;
+    private $companyId;
+    private $companyIdAttributes;
+    private $companyLegalForm;
+
+    /**
+     * Seller name
+     */
+    public function getRegistrationNumber(): ?string {
+       return $this->registrationName;
+    }
+
+    /**
+     * Set seller name;
+     */
+    public function setRegistrationNumber(?string $registrationName): LegalEntity {
+       return $this->registrationName;
+    }
+
+    /**
+     * Seller legal registration identifier
+     */
+    public function getCompanyId(): ?string {
+        return $this->companyId;
+    }
+
+    /**
+     * set Company ID
+     */
+    public function setCompanyId(?string $companyId, $attributes = null): LegalEntity {
+        $this->companyId = $companyId;
+        if(isset($attributes)) {
+          $this->$companyIdAttributes = $attributes;
+        }
+        return $this;
+    }
+
+    /**
+     * Company form legal
+     */
+    public function getCompanyLegalForm(): ?string {
+        return $this->companyLegalForm;
+    }
+
+    /**
+     * Set company form legal
+     */
+    public function setCompanyLegal(?string $companyLegalForm): LegalEntity {
+        $this->companyLegalForm = $companyLegalForm;
+        return $this;
+    }
 }
