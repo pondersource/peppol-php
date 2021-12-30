07cd522136a74b04395e910c96b08f21ffa9a4dd
ishifoev <alex.malikov94@gmail.com>
2021-12-30 09:07:38 +0500

    Working with Readme and start work with Price and TaxCategory

---
+++ experiments/invoice-ubl/src/README.md
@@ -0,0 +1,30 @@
+## UBL Invoice
+
+### UBL Address and Attachment
+
+<img src="https://github.com/pondersource/peppol-php/blob/main/experiments/invoice-ubl/src/pics/diagram_ubl_start.PNG?raw=true"/>
+
+### Unit Code 
+We have more unit code that need fill. I created the class for it. But we can add more unit codes. Feel free create PR.
+
+- [Unit Code](https://docs.peppol.eu/poacc/billing/3.0/codelist/UNECERec20/)
+
+### Invoice Type Code 
+A code specifying the functional type of the Invoice. The base value for send invoice is 380. But you can see more code below in link.
+
+- [Invoice Type Code](https://docs.peppol.eu/poacc/billing/3.0/codelist/UNCL1001-inv/)
+
+### UBL Party
+
+<img src="https://github.com/pondersource/peppol-php/blob/main/experiments/invoice-ubl/src/pics/ubl-party.PNG?raw=true"/>
+
+### UBL Payments
+
+<img src="https://github.com/pondersource/peppol-php/blob/main/experiments/invoice-ubl/src/pics/ubl-payment.PNG?raw=true"/>
+
+### Duty or tax or fee category code(Subset of UNCL5305)
+- [Vat Categrory Code](https://docs.peppol.eu/poacc/billing/3.0/codelist/UNCL5305/)
+
+### UBL Item with classified tax
+
+<img src="https://github.com/pondersource/peppol-php/blob/ubl-invoice/experiments/invoice-ubl/src/pics/ubl-item.PNG?raw=true"/>
---
+++ experiments/invoice-ubl/src/index.php
@@ -0,0 +1,101 @@
+<?php
+
+require 'Country.php';
+require 'PostalAddress.php';
+require 'Party.php';
+require 'PartyTaxScheme.php';
+require 'Contact.php';
+require 'LegalMonetaryTotal.php';
+require 'TaxScheme.php';
+require 'FinancialInstitutionBranch.php';
+require 'PayeeFinancialAccount.php';
+require 'PaymentMeans.php';
+require 'LegalEntity.php';
+require 'ClassifiedTaxCategory.php';
+require 'Item.php';
+require 'UnitCode.php';
+require 'Price.php';
+
+$url = 'https://docs.oasis-open.org/ubl/os-UBL-2.1/xsd/maindoc/UBL-Invoice-2.1.xsd';
+ // Tax scheme
+ $taxScheme = (new TaxScheme())
+ ->setId('VAT');
+
+$country = (new Country())
+            ->setIdentificationCode('NL');
+
+        // Full address
+$address = (new PostalAddress())
+                ->setStreetName('Lisk Center Utreht')
+                ->setAddionalStreetName('De Burren')
+                ->setCityName('Utreht')
+                ->setPostalZone('3521')
+                ->setCountry($country);
+
+$financialInstitutionBranch = (new FinancialInstitutionBranch())
+                ->setId('RABONL2U');
+    
+$payeeFinancialAccount = (new PayeeFinancialAccount())
+               ->setFinancialInstitutionBranch($financialInstitutionBranch)
+                ->setName('Customer Account Holder')
+                ->setId('NL00RABO0000000000');
+$paymentMeans = (new PaymentMeans())
+                ->setPayeeFinancialAccount($payeeFinancialAccount)
+                ->setPaymentMeansCode(31, [])
+                ->setPaymentId('our invoice 1234');
+
+ // Supplier company node
+ $supplierLegalEntity = (new LegalEntity())
+ ->setRegistrationNumber('PonderSource')
+ ->setCompanyId('NL123456789');
+
+$supplierPartyTaxScheme = (new PartyTaxScheme())
+ ->setTaxScheme($taxScheme)
+ ->setCompanyId('NL123456789');
+
+$supplierCompany = (new Party())
+ ->setPartyName('PonderSource')
+ ->setLegalEntity($supplierLegalEntity)
+ ->setPartyTaxScheme($supplierPartyTaxScheme)
+ ->setPostalAddress($address);
+
+
+
+// Client company node
+$clientLegalEntity = (new LegalEntity())
+ ->setRegistrationNumber('Client Company Name')
+ ->setCompanyId('Client Company Registration');
+
+$clientPartyTaxScheme = (new PartyTaxScheme())
+ ->setTaxScheme($taxScheme)
+ ->setCompanyId('BE123456789');
+
+$clientCompany = (new Party())
+ ->setPartyName('Client Company Name')
+ ->setLegalEntity($clientLegalEntity)
+ ->setPartyTaxScheme($clientPartyTaxScheme)
+ ->setPostalAddress($address);
+
+$legalMonetaryTotal = (new LegalMonetaryTotal())
+ ->setPayableAmount(10 + 2.1)
+ ->setAllowanceTotalAmount(0)
+ ->setTaxInclusiveAmount(10 + 2.1)
+ ->setLineExtensionAmount(10)
+ ->setTaxExclusiveAmount(10);
+
+ $classifiedTaxCategory = (new ClassifiedTaxCategory())
+ ->setId('S')
+ ->setPercent(21.00)
+ ->setTaxScheme($taxScheme);
+
+  // Product
+  $productItem = (new Item())
+  ->setName('Product Name')
+  ->setClassifiedTaxCategory($classifiedTaxCategory)
+  ->setDescription('Product Description');
+
+// Price
+ $price = (new Price())
+       ->setBaseQuantity(1)
+       ->setUnitCode(UnitCode::UNIT)
+       ->setPriceAmount(10);
