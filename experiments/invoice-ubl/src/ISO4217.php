1f6e6aa859feae1095d4ab9bfd0b17b227dc1961
ishifoev <alex.malikov94@gmail.com>
2021-12-31 12:09:58 +0500

    Working with ISO code

---
+++ experiments/invoice-ubl/src/ISO4217.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * The currency used for VAT accounting 
+ * and reporting purposes as accepted or required in the country of the Seller.
+ */
+const AED = 'AED';
+const AFN = 'AFN';
+const ALL = 'ALL';
+const AMD = 'AMD';
+const ANG = 'ANG';
+const AOA = 'AOA';
+const ARS = 'ARS';
+const AUD = 'AUD';
+const AWG = 'AWG';
+const AZN = 'AZN';
+const BAM = 'BAM';
+const BBD = 'BBD';
+const BDT = 'BDT';
+const BGN = 'BGN';
+const BHD = 'BHD';
+const BIF = 'BIF';
+const BMD = 'BMD';
