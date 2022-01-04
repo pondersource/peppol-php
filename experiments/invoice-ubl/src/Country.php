03e7ea2697386778f8cd12c802154987299059f6
ishifoev <alex.malikov94@gmail.com>
2022-01-04 15:23:37 +0500

    This is working for Address and Country serialize

---
+++ experiments/invoice-ubl/src/Country.php
@@ -0,0 +1,29 @@
+<?php
+
+use Sabre\Xml\Writer;
+use Sabre\Xml\XmlSerializable;
+
+class Country implements XmlSerializable {
+    private $identificationCode;
+
+    /**
+     * Get Country Code
+     */
+    public function getIdentificationCode(): ?string {
+        return $this->identificationCode;
+    }
+
+    /**
+     * Set Country Code
+     */
+    public function setIdentificationCode(?string $identificationCode): Country {
+        $this->identificationCode = $identificationCode;
+        return $this;
+    }
+
+    public function xmlSerialize(Writer $writer) {
+        $writer->write([
+            Schema::CBC . 'IdentificationCode' => $this->identificationCode
+        ]);
+    }
+}
