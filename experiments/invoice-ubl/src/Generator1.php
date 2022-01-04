d0e11bd46b88a54cce0c82a8acb89b31617c5b19
ishifoev <alex.malikov94@gmail.com>
2022-01-04 15:23:37 +0500

    This is working for Address and Country serialize

---
+++ experiments/invoice-ubl/src/Generator1.php
@@ -0,0 +1,25 @@
+<?php
+
+use Sabre\Xml\Service;
+
+class Generator1
+{
+    public static $currencyID;
+
+    public static function invoice(PostalAddress $address, $currencyId = 'EUR')
+    {
+        self::$currencyID = $currencyId;
+
+        $xmlService = new Service();
+
+        $xmlService->namespaceMap = [
+            'urn:oasis:names:specification:ubl:schema:xsd:Invoice-2' => '',
+            'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2' => 'cbc',
+            'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2' => 'cac'
+        ];
+
+        return $xmlService->write('PostalAddress', [
+            $address
+        ]);
+    }
+}
