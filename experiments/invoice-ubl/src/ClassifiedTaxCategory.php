d0c969902e20aa26e9ad68c5c540474d4b4ba84f
ishifoev <alex.malikov94@gmail.com>
2022-01-05 11:38:52 +0500

    Working with Item, LegalMonetaryTotal and ClassifiedTaxcategory

--- experiments/invoice-ubl/src/ClassifiedTaxCategory.php
+++ experiments/invoice-ubl/src/ClassifiedTaxCategory.php
@@ -1,17 +1,37 @@
 <?php
 
+
+use Sabre\Xml\Writer;
+use Sabre\Xml\XmlSerializable;
+
 use InvalidArgumentException as InvalidArgumentException;
 
-class ClassifiedTaxCategory {
+class ClassifiedTaxCategory implements XmlSerializable {
     private $id;
     private $percent;
     private $taxScheme;
+    private $schemeID;
+    private $schemeName;
 
     /**
      * Invoiced item VAT category code
      */
     public function getId(): ?string {
-       return $this->id;
+        if (!empty($this->id)) {
+            return $this->id;
+        }
+
+        if ($this->getPercent() !== null) {
+            if ($this->getPercent() >= 21) {
+                return VatCategoryCode::STANDART_RATE;
+            } elseif ($this->getPercent() <= 21 && $this->getPercent() >= 6) {
+                return VatCategoryCode::VAT_REVERSE_CHANGE;
+            } else {
+                return VatCategoryCode::ZERO_RATE_GOODS;
+            }
+        }
+
+        return null;
     }
 
     /**
@@ -63,6 +83,38 @@ class ClassifiedTaxCategory {
 
         if ($this->getPercent() === null) {
             throw new InvalidArgumentException('Missing taxcategory percent');
+        }
+    }
+
+    /**
+     * Serialize Classified Tax Category
+     */
+    public function xmlSerialize(Writer $writer) {
+        $this->validate();
+
+        $schemeAttributes = [];
+        if($this->schemeID !== null) {
+            $schemeAttributes['schemeID'] = $this->schemeID;
+        }
+        if($this->schemeName !== null) {
+            $schemeAttributes['schemeName'] = $this->schemeName;
+        }
+
+        $writer->write([
+            [
+                'name' => Schema::CBC . 'ID',
+                'value' => $this->getId(),
+                'attributes' => $schemeAttributes
+            ],
+            Schema::CBC . 'Percent' => number_format($this->percent, 2, '.', ''),
+        ]);
+
+        if ($this->taxScheme !== null) {
+            $writer->write([Schema::CAC . 'TaxScheme' => $this->taxScheme]);
+        } else {
+            $writer->write([
+                Schema::CAC . 'TaxScheme' => null,
+            ]);
         }
     }
 }
