3afdcb7c64d3e465b2260aa8923a3ebf56b15fbd
bänz <benz.schenk@brokkoli.be>
2022-01-05 10:54:57 +0100

    little toy client implementation, doesn't really work yet, but getting there

--- experiments/client-3/soapClient.php
+++ experiments/client-3/soapClient.php
@@ -1,14 +1,50 @@
 <?php
-$example = file_get_contents('./base-example.xml');
+include('./vendor/autoload.php');
 
-file_exists('./config.php') && include('./config.php');
+use GuzzleHttp\Client;
 
-$serverUrl?:$serverUrl='http://localhost:8080';
+$invoice = file_get_contents('./base-example.xml');
+$soapHeader = file_get_contents('./soapHeader.xml');
+$mimeBoundary = "----=PeppolPhpSoapBoundary";
+$cipher = "aes-128-gcm";
+$key = 'peppol'; //?
+$tag = 'peppoltag123456789'; //?
 
-$client = new SoapClient(null, array(
-	'location' => $serverUrl,
-	'uri' => $serverUrl,
-	'trace' => 1));
-$return = $client->__soapCall("invoice",[$example]);
-echo $return;
-?>
+$client = new Client([
+	'base_uri' => 'http://localhost:8080/as4',
+	'headers' => [
+		'MIME-Version' => '1.0',
+		'Content-Type' => "multipart/related; boundary='$mimeBoundary'; type='application/soap+xml'; charset=UTF-8"
+	]]);
+
+function mimePart($boundary, $headers, $content){
+	$res = $boundary . "\n";
+	foreach($headers as $key => $value){
+		$res = $res . $key . ': ' . $value . "\n";
+	}
+	$res = $res . $content;
+}
+
+function preparePayload($content) {
+	$cipher = "aes-128-gcm";
+	$key = "peppol";
+	$tag = "peppoltag";
+	$zip = gzcompress($content);
+	$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher));
+	return openssl_encrypt($zip,$cipher,$key,0,$iv,$tag);
+	return '';
+}
+
+$requestString = mimePart($mimeBoundary, 
+						  ['Content-Type' => 'application/soap+xml;charset=UTF-8', 
+						   'Content-Transfer-Encoding' => 'binary'], 
+						   $soapHeader) .
+				 mimePart($mimeBoundary, 
+				 		  ['Content-Type' => 'application/octet-stream',
+						   'Content-Transfer-Encoding' => 'binary', 
+						   'Content-Description' => 'Attachment', 
+						   'Content-ID' => '<phase4-att-7abd0eda-6da5-477f-b43c-ba90ea347169@cid>'], 
+						   preparePayload($invoice));
+
+$r = $client->request('POST','http://localhost:8080/as4',['body' => $requestString]);
+echo json_encode($r);
