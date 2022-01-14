e4d1213918c38854059a56f365472e7dc6ba30f8
bänz <benz.schenk@brokkoli.be>
2022-01-14 10:54:18 +0100

    Added classes and functions to generate requests like phase4-peppol-client

---
+++ experiments/client/SoapMessage.php
@@ -0,0 +1,49 @@
+<?php
+namespace PonderSource\Peppol;
+require_once('vendor/autoload.php');
+require_once('ElectronicBusinessMessage.php');
+require_once('GUID.php');
+use Sabre\Xml\Writer;
+use Sabre\Xml\XmlSerializable;
+
+class SoapMessage implements XmlSerializable {
+
+	const S12 = 'http://www.w3.org/2003/05/soap-envelope';
+	const WSU = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd';
+	const EB = 'http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/';
+
+	private ElectronicBusinessMessage $ebms;
+	private string $bodyId;
+
+	function __construct($ebms) {
+		$this->ebms = $ebms;
+		$this->bodyId = GUID();
+	}
+
+	function getEBMS(): ElectronicBusinessMessage {
+		return $this->ebms;
+	}
+
+	function setEBMS($ebms): SoapMessage {
+		$this->ebms = $ebms;
+		return $this;
+	}
+
+	function getBodyId(): string {
+		return $this->bodyId;
+	}
+
+	function XmlSerialize(Writer $writer) {
+		$writer->write([
+			'{' . $this::S12 . '}Header' => [
+				'{' . $this::EB . '}Messaging' => $this->ebms
+			],
+			[
+				'name' => '{' . $this::S12 . '}Body',
+				'attributes' => [
+					'{' . $this::WSU . '}Id' => $this->bodyId
+				],
+			],
+		]);
+	}
+}
