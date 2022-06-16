cedbc015fc616c375815d3cd8cfc077d7086fda2
Your Name <you@example.com>
2022-06-16 14:53:57 +0000

    ncp1/2 start scripts

---
+++ scripts/start-ncp1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria1.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
+docker run -d --network=testnet --name=ncp1.docker -v /root/peppol-php/nextcloud-app/peppolnext:/var/www/html/apps/peppolnext ncp1
+
+sleep 15
+docker exec -e DBHOST=maria1.docker -e USER=einstein -e PASS=relativity  -u www-data ncp1.docker sh /init.sh
+docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek nextcloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://revanc1.docker/');"
+docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek nextcloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'revaSharedSecret', 'shared-secret-1');"
