6f9aa7551b4fbd1b8b4059d9515024cf19708265
root <root@mesh.pondersource.org>
2022-10-05 13:37:31 +0000

    make receiver.cer accessible for PHP sender

--- scripts/transportp12.sh
+++ scripts/transportp12.sh
@@ -2,3 +2,4 @@
 set -e
 docker run -v `pwd`/p12transport:/p12transport phase4-client cp /root/phase4/phase4-peppol-server-webapp/keys/test.p12 /p12transport/
 docker run -v `pwd`/p12transport:/p12transport phase4-client cp /root/sender.cer /p12transport/
+docker run -v `pwd`/p12transport:/p12transport phase4-client cp /root/receiver.cer /p12transport/
