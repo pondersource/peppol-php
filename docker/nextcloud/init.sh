9f446067bbd9b3e43a10d803fe45bcf8cd83268a
Michiel de Jong <michiel@unhosted.org>
2022-10-11 16:42:42 +0200

    allowed domains

--- docker/nextcloud/init.sh
+++ docker/nextcloud/init.sh
@@ -3,7 +3,7 @@ php console.php app:enable peppolnext
 php console.php app:enable groupfolders
 sed -i "8 i\      1 => 'nc1.docker'," /var/www/html/config/config.php
 sed -i "9 i\      2 => 'nc2.docker'," /var/www/html/config/config.php
-sed -i "10 i\      3 => 'nc1.pondersource.net'," /var/www/html/config/config.php
-sed -i "11 i\      4 => 'nc2.pondersource.net'," /var/www/html/config/config.php
+sed -i "10 i\      3 => 'c2.pondersource.net'," /var/www/html/config/config.php
+sed -i "11 i\      4 => 'c3.pondersource.net'," /var/www/html/config/config.php
 sed -i "3 i\  'allow_local_remote_servers' => true," config/config.php
 sed -i "4 i\  'debug' => true," config/config.php
