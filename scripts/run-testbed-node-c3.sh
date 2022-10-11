51e1d3156e69bfec3c0b6a85d04f83ffbc960b22
Michiel de Jong <michiel@unhosted.org>
2022-10-11 16:13:59 +0200

    prepare to run nc on c2 and c3

--- scripts/run-testbed-node-c3.sh
+++ scripts/run-testbed-node-c3.sh
@@ -2,7 +2,7 @@
 set -e
 echo Mounting peppol-php code repo from the host, folder: $PEPPOL_PHP_DIR 
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
-docker run -d --network=testnet --name=nc -p 443:443 -p 80:80 -v $PEPPOL_PHP_DIR:/var/www/html/apps/peppol-php c3
+docker run -d --network=testnet --name=nc -p 443:443 -p 80:80 -v /root/tls:/tls -v $PEPPOL_PHP_DIR:/var/www/html/apps/peppol-php c3
 docker exec -w /var/www/html/apps/peppolnext nc make composer
 
 echo "sleeping for 15 seconds"
