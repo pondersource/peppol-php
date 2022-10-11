69bd40bb63e54169d7acd885ad24ecfad987a0c2
root <root@mesh.pondersource.org>
2022-10-11 13:10:45 +0000

    start with #138

---
+++ scripts/run-testbed-node-c3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+echo Mounting peppol-php code repo from the host, folder: $PEPPOL_PHP_DIR 
+docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
+docker run -d --network=testnet --name=nc -p 443:443 -p 80:80 -v $PEPPOL_PHP_DIR:/var/www/html/apps/peppol-php c3
+docker exec -w /var/www/html/apps/peppolnext nc make composer
+
+echo "sleeping for 15 seconds"
+sleep 15
+echo "slept for 15 seconds"
+
+docker exec -it -e DBHOST=maria -e USER=marie -e PASS=radioactivity -u www-data nc sh /init.sh
--- scripts/start-to-Nextcloud.sh
+++ scripts/start-to-Nextcloud.sh
@@ -2,7 +2,7 @@
 set -e
 echo Mounting peppol-php code repo from the host, folder: $PEPPOL_PHP_DIR 
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria2.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
-docker run -d --network=testnet --name=nc2.docker -v `pwd`/p12transport:/p12transport -v $PEPPOL_PHP_DIR:/var/www/html/apps/peppol-php nc2
+docker run -d --network=testnet --name=nc2.docker -v $PEPPOL_PHP_DIR/p12transport:/p12transport -v $PEPPOL_PHP_DIR:/var/www/html/apps/peppol-php nc2
 docker exec -w /var/www/html/apps/peppolnext nc2.docker make composer
 
 echo "sleeping 15 seconds"
