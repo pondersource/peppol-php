93b8a78077b0baa6a437d7aeec9611b5c93d6fbf
root <root@c3.pondersource.net>
2022-10-11 13:45:04 +0000

    work on logger

--- docker/logger/index.js
+++ docker/logger/index.js
@@ -1,1 +1,10 @@
+import { createServer } from "https";
+import { readFileSync } from "fs";
 console.log("it works");
+const server = https.createServer({
+	key: readFileSync("/tls/privkey.pem"),
+	cert: readFileSync("/tls/fullchain.pem")
+}, (req, res) => {
+	res.end("hello");
+});
+server.listen(443);
