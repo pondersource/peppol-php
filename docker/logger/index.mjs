7313d1af3a8fe0f582f66cdc06b21f83b20881f3
root <root@c3.pondersource.net>
2022-10-11 13:54:36 +0000

    log method, url and request body

--- docker/logger/index.mjs
+++ docker/logger/index.mjs
@@ -1,10 +1,19 @@
 import { createServer } from "https";
 import { readFileSync } from "fs";
 console.log("it works");
-const server = https.createServer({
+const server = createServer({
 	key: readFileSync("/tls/privkey.pem"),
 	cert: readFileSync("/tls/fullchain.pem")
 }, (req, res) => {
+	console.log(req.method, req.url);
+	req.on('data', (chunk) => {
+		console.log('CHUNK', chunk.toString());
+	});
+	
+	req.on('end', (chunk) => {
+		console.log('END');;
+	});
+	
 	res.end("hello");
 });
 server.listen(443);
