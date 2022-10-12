fe29d580632de1613d84f2f31f8a9003acabc610
Michiel de Jong <michiel@unhosted.org>
2022-10-12 10:26:41 +0200

    run nc behind logger

--- docker/logger/index.mjs
+++ docker/logger/index.mjs
@@ -1,19 +1,28 @@
 import { createServer } from "https";
 import { readFileSync } from "fs";
 console.log("it works");
+const SERVER = "http://server/index.php/apps/peppolnext/api/v1";
+
 const server = createServer({
 	key: readFileSync("/tls/privkey.pem"),
 	cert: readFileSync("/tls/fullchain.pem")
-}, (req, res) => {
-	console.log(req.method, req.url);
-	req.on('data', (chunk) => {
+}, (reqIn, resIn) => {
+	let body = "";
+	console.log(reqIn.method, reqIn.url);
+	reqIn.on('data', (chunk) => {
 		console.log('CHUNK', chunk.toString());
+		body += chunk.toString();
 	});
 	
-	req.on('end', (chunk) => {
+	reqIn.on('end', async (chunk) => {
 		console.log('END');;
+		const reqOut = await fetch(SERVER, {
+			method: "POST",
+			body
+		});
+    const resBody = await reqOut.text();
+		console.log("RESP", resBody);
+		resIn.end(resBody);
 	});
-	
-	res.end("hello");
 });
 server.listen(443);
--- scripts/run-testbed-node-c2.sh
+++ scripts/run-testbed-node-c2.sh
@@ -2,8 +2,9 @@
 set -e
 echo Mounting peppol-php code repo from the host, folder: $PEPPOL_PHP_DIR 
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
-docker run -d --network=testnet --name=nc -p 443:443 -p 80:80 -v /root/tls:/tls -v $PEPPOL_PHP_DIR:/var/www/html/apps/peppol-php c2
+docker run -d --network=testnet --name=nc -v /root/tls:/tls -v $PEPPOL_PHP_DIR:/var/www/html/apps/peppol-php c2
 docker exec -w /var/www/html/apps/peppolnext nc make composer
+docker run -d --network=testnet -p 443:443 -v /root/tls:/tls -v $PEPPOL_PHP_DIR:/var/www/html/apps/peppol-php logger
 
 echo "sleeping for 15 seconds"
 sleep 15
