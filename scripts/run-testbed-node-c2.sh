cf4da66ff27e2c47d94c30841696b885a6881ff3
root <root@c2.pondersource.net>
2022-10-13 12:59:08 +0000

    debugging mime parsing

--- scripts/run-testbed-node-c2.sh
+++ scripts/run-testbed-node-c2.sh
@@ -4,7 +4,7 @@ echo Mounting peppol-php code repo from the host, folder: $PEPPOL_PHP_DIR
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 docker run -d --network=testnet --name=nc1.docker -v /root/tls:/tls -v $PEPPOL_PHP_DIR:/var/www/html/apps/peppol-php c2
 docker exec -w /var/www/html/apps/peppolnext nc1.docker make composer
-docker run -d --network=testnet -p 443:443 -v /root/tls:/tls -v $PEPPOL_PHP_DIR:/var/www/html/apps/peppol-php logger
+docker run --rm --name=logger -d --network=testnet -p 443:443 -v /root/tls:/tls -v $PEPPOL_PHP_DIR:/var/www/html/apps/peppol-php logger
 
 echo "sleeping for 15 seconds"
 sleep 15
