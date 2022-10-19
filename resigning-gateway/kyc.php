70da377fee97d22730f464f2b826d4b11b2133f7
root <root@mesh.pondersource.org>
2022-10-19 14:56:28 +0000

    Add path http://resigning-gateway/kyc.php, ref #151

---
+++ resigning-gateway/kyc.php
@@ -0,0 +1,46 @@
+<html>
+  <body>
+    <h2>Tell us your VAT number</h2>
+    <p>Please choose the country in which your organisation pays VAT:</p>
+    <ul>
+      <li id="AD:VAT">Andorra</li>
+      <li id="AL:VAT">Albania</li>
+      <li id="AT:VAT">Austria</li>
+      <li id="BE:VAT">Belgium</li>
+      <li id="BG:VAT">Bulgaria</li>
+      <li id="CH:VAT">Switzerland</li>
+      <li id="CY:VAT">Cyprus</li>
+      <li id="CZ:VAT">Czechia</li>
+      <li id="DE:VAT">Germany</li>
+      <li id="EE:VAT">Estonia</li>
+      <li id="ES:VAT">Spain</li>
+      <li id="FR:VAT">France</li>
+      <li id="GB:VAT">Great Britain</li>
+      <li id="GR:VAT">Greece</li>
+      <li id="HR:VAT">Croatia</li>
+      <li id="HU:VAT">Hungary</li>
+      <li id="IE:VAT">Ireland</li>
+      <li id="IT:VAT">Italy</li>
+      <li id="LI:VAT">Liechtenstein</li>
+      <li id="LT:VAT">Lithuania</li>
+      <li id="LU:VAT">Luxemburg</li>
+      <li id="LV:VAT">Latvia</li>
+      <li id="MC:VAT">Monaco</li>
+      <li id="ME:VAT">Montenegro</li>
+      <li id="MK:VAT">Macedonia</li>
+      <li id="MT:VAT">Malta</li>
+      <li id="NL:VAT">The Netherlands</li>
+      <li id="NO:VAT">Norway</li>
+      <li id="PL:VAT">Poland</li>
+      <li id="PT:VAT">Portugal</li>
+      <li id="RO:VAT">Romania</li>
+      <li id="RS:VAT">Serbia</li>
+      <li id="SE:VAT">Sweden</li>
+      <li id="SI:VAT">Slovenia</li>
+      <li id="SK:VAT">Slovakia</li>
+      <li id="SM:VAT">San</li> Marino
+      <li id="TR:VAT">Turkey</li>
+      <li id="VA:VAT">Vatican City</li>
+    <ul>
+  </body>
+</html>
