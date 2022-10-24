5287361d63c3215d6f456613eb857699ae2937bd
Michiel de Jong <michiel@unhosted.org>
2022-10-24 09:54:02 +0200

    pull in chances from gh:pondersource/connectyourbooks.com

--- re-signing-gateway/index.php
+++ re-signing-gateway/index.php
@@ -3,29 +3,65 @@
 // SPDX-License-Identifier: MIT
 
 <?php
-
 require_once("./lib/PonderSource/AS4.php");
-
-error_log("Hi");
-$as4 = new \OCA\PeppolNext\PonderSource\AS4();
-$contentType = "unknown";
-$headers = apache_request_headers();
-foreach ($headers as $header => $value) {
-  if (strtolower($header) == "content-type") {
-    $contentType = $value;
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+  error_log("Hi");
+  $as4 = new \OCA\PeppolNext\PonderSource\AS4();
+  $contentType = "unknown";
+  $headers = apache_request_headers();
+  foreach ($headers as $header => $value) {
+    if (strtolower($header) == "content-type") {
+      $contentType = $value;
+    }
+    error_log("[INCOMING HEADER] $header: $value");
   }
-  error_log("[INCOMING HEADER] $header: $value");
+  error_log("[CONTENT-TYPE] $contentType");
+  $body = file_get_contents('php://input');
+  error_log("[REQUEST BODY LENGTH] " . strlen($body));
+  $response = $as4->handleAs4($contentType, $body);
+  error_log("[RESPONSE BODY LENGTH] " . strlen($response));
+  header('Referrer-Policy: strict-origin-when-cross-origin');
+  header('X-Frame-Options: SAMEORIGIN');
+  header('X-Content-Type-Options: nosniff');
+  header('X-XSS-Protection: 1; mode=block');
+  header('Strict-Transport-Security: max-age=3600;includeSubDomains');
+  header('Cache-Control: no-cache, no-store, must-revalidate, proxy-revalidate');
+  header('Content-Type: application/soap+xml;charset=utf-8');
+  header('Content-Disposition:');
+  echo $response;
+} else {
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Connect your Books!</title>
+    <style>
+      body { background-color: #A3DDDF }
+      header { width: 40em; margin: 5em auto; color: white }
+      article { width: 40em; margin: 5em auto; padding: 3em; border-radius: 1em; background-color: #FFE4B5 }
+      article pre { background-color: grey; padding: 3em; color: yellow; border-radius: 1em; text-align: center }
+      footer { width: 40em; margin: 5em auto; color: white }
+    </style>
+  </head>
+  <body>
+    <header>
+      <h2>Connect your Books!</h2>
+    </header>
+    <article>
+      <p>We at <a href="https://pondersource.com">Ponder Source</a> are working on a product that will allow bookkeeping systems to be connected
+      to each other, in the vision of <a href="https://federatedbookkeeping.org">Federated Bookkeeping</a>.</p>
+      <p>For this, we will combine a number of technologies:</p>
+      <ul>
+        <li><a href="https://peppol.eu">Peppol</a> and <a href="https://github.com/pondersource/peppol-php/blob/main/docs/ngi-assure-proposal.md">AS4+WebID</a> for e-invoicing</li>
+        <li><a href="https://www.w3.org/community/timesh/">Federated Timesheets</a></li>
+        <li>Integration of all the <a href="https://github.com/federatedbookkeeping/research/blob/main/api.md">proprietary billing APIs</a> we can find</li>
+        <li>Integration of all the bookkeeping software platforms we can find</li>
+        <li>Novel <a href="https://prejournal.org">Prejournal</a> bookkeeping techniques</li>
+      </ul>
+      <p>The HTML code of this website is maintained through a <a href="https://github.com/pondersource/connectyourbooks.com">GitHub repo</a>.</p>
+    </article>
+  </body>
+</html>
+<?php
 }
-error_log("[CONTENT-TYPE] $contentType");
-$body = file_get_contents('php://input');
-error_log("[BODY] $body");
-$response = $as4->handleAs4($contentType, $body);
-header('Referrer-Policy: strict-origin-when-cross-origin');
-header('X-Frame-Options: SAMEORIGIN');
-header('X-Content-Type-Options: nosniff');
-header('X-XSS-Protection: 1; mode=block');
-header('Strict-Transport-Security: max-age=3600;includeSubDomains');
-header('Cache-Control: no-cache, no-store, must-revalidate, proxy-revalidate');
-header('Content-Type: application/soap+xml;charset=utf-8');
-header('Content-Disposition:');
-echo $response;
+?>
--- re-signing-gateway/kyc.php
+++ re-signing-gateway/kyc.php
@@ -2,131 +2,164 @@
 //
 // SPDX-License-Identifier: MIT
 
+<html>
+  <head>
+  <style>
+      body { background-color: #A3DDDF }
+      header { width: 40em; margin: 5em auto; color: white }
+      article { width: 40em; margin: 5em auto; padding: 3em; border-radius: 1em; background-color: #FFE4B5 }
+      article pre { background-color: grey; padding: 3em; colour: yellow; border-radius: 1em; text-align: center }
+      footer { width: 40em; margin: 5em auto; color: white }    </style>
+  </head>
+  <body>
 <?php
   require_once(dirname(__FILE__) . "/vendor/autoload.php");
   use phpseclib3\File\X509;
 
   error_log("User chose" . var_export($_POST, true));
-?>
-<html>
-  <body>
-    <h2>Tell us your VAT number</h2>
-    <p></p>
-    <form action="" method="post">
-      <div>
-        <label for="cc">Please choose the country in which your organisation pays VAT:</label>
 
-        <select id="cc" name="cc" required>
-<?php
-  $countries = [
-    "??" => " --- please select ---",
-    "AD" => "Andorra",
-    "AT" => "Austria",
-    "BE" => "Belgium",
-    "BG" => "Bulgaria",
-    "CH" => "Switzerland",
-    "CY" => "Cyprus",
-    "CZ" => "Czechia",
-    "DE" => "Germany",
-    "EE" => "Estonia",
-    "ES" => "Spain",
-    "FR" => "France",
-    "GB" => "Great Britain",
-    "GR" => "Greece",
-    "HR" => "Croatia",
-    "HU" => "Hungary",
-    "IE" => "Ireland",
-    "IT" => "Italy",
-    "LI" => "Liechtenstein",
-    "LT" => "Lithuania",
-    "LU" => "Luxemburg",
-    "LV" => "Latvia",
-    "MC" => "Monaco",
-    "ME" => "Montenegro",
-    "MK" => "Macedonia",
-    "MT" => "Malta",
-    "NL" => "The Netherlands",
-    "NO" => "Norway",
-    "PL" => "Poland",
-    "PT" => "Portugal",
-    "RO" => "Romania",
-    "RS" => "Serbia",
-    "SE" => "Sweden",
-    "SI" => "Slovenia",
-    "SK" => "Slovakia",
-    "SM" => "San</optio",
-    "TR" => "Turkey",
-    "VA" => "Vatican City",
-  ];
-  $selected = (isset($_POST["cc"]) ? $_POST["cc"] : "NL" );
-  $vatnum = (isset($_POST["vatnum"]) ? $_POST["vatnum"] : "862637223B01" );
-  $server = (isset($_POST["server"]) ? $_POST["server"] : "https://cloud.pondersource.org" );
-  $path = (isset($_POST["path"]) ? $_POST["path"] : "/index.php/apps/peppolnext/cert" );
-  foreach($countries as $cc => $countryName) {
-    echo "          <option value=\"$cc\" " .
-     ($cc == $selected ? "selected" : "" ) .
-    ">$countryName</option>\n";
+  function getCountries() {
+    return [
+      "??" => " --- please select ---",
+      "AD" => "Andorra",
+      "AT" => "Austria",
+      "BE" => "Belgium",
+      "BG" => "Bulgaria",
+      "CH" => "Switzerland",
+      "CY" => "Cyprus",
+      "CZ" => "Czechia",
+      "DE" => "Germany",
+      "EE" => "Estonia",
+      "ES" => "Spain",
+      "FR" => "France",
+      "GB" => "Great Britain",
+      "GR" => "Greece",
+      "HR" => "Croatia",
+      "HU" => "Hungary",
+      "IE" => "Ireland",
+      "IT" => "Italy",
+      "LI" => "Liechtenstein",
+      "LT" => "Lithuania",
+      "LU" => "Luxemburg",
+      "LV" => "Latvia",
+      "MC" => "Monaco",
+      "ME" => "Montenegro",
+      "MK" => "Macedonia",
+      "MT" => "Malta",
+      "NL" => "The Netherlands",
+      "NO" => "Norway",
+      "PL" => "Poland",
+      "PT" => "Portugal",
+      "RO" => "Romania",
+      "RS" => "Serbia",
+      "SE" => "Sweden",
+      "SI" => "Slovenia",
+      "SK" => "Slovakia",
+      "SM" => "San</optio",
+      "TR" => "Turkey",
+      "VA" => "Vatican City",
+    ];
   }
-  ?>
-        </select>
-      </div>
-      <div>
-        <label for="vatnum">Enter your VAT number</label>
-        <input type="text" name="vatnum" value="<?php  echo $vatnum; ?>"/>
-      </div>
-      <div>
-        <label for="server">Enter the URL of your bookkeeping server:</label>
-        <input type="text" name="server" style="width:50em;" value="<?php  echo $server; ?>"/>
-      </div>
-      <div>
-        <label for="path">Enter your server's cert path:</label>
-        <input type="text" name="path" style="width:50em;" value="<?php  echo $path; ?>"/>
-      </div>
-      <div>
-        <input type="submit" value="Check" />
-      </div> 
-    </form>
-    <div>
+  function displayForm() {
+    $selected = (isset($_POST["cc"]) ? $_POST["cc"] : "NL");
+    $vatnum = (isset($_POST["vatnum"]) ? $_POST["vatnum"] : "862637223B01");
+    $webid = (isset($_POST["webid"]) ? $_POST["webid"] : "pondersource.com/id");
+?>
+    <header>
+      <h2>Tell us your VAT number</h2>
+    </header>
+    <article>
+      <p></p>
+      <form action="" method="post">
+        <div>
+          <label for="cc"><h3>Please choose the country in which your organisation pays VAT:<h3></label>
+
+          <select id="cc" name="cc" required>
 <?php
-if (isset($_POST["cc"]) && isset($_POST["vatnum"])) {
+    $countries = getCountries();
+    foreach ($countries as $cc => $countryName) {
+        echo "          <option value=\"$cc\" " .
+         ($cc == $selected ? "selected" : "") .
+        ">$countryName</option>\n";
+    }
+?>
+          </select>
+        </div>
+        <div>
+          <label for="vatnum"><h3>Enter your VAT number<h3></label>
+          <input type="text" name="vatnum" value="<?php  echo $vatnum; ?>"/>
+        </div>
+        <div>
+          <label for="server"><h3>Enter your WebID:<h3></label>
+          <input type="text" name="webid" value="<?php  echo $webid; ?>"/>
+        </div>
+        <div>
+          <input type="submit" value="Check" />
+        </div> 
+      </form>
+      <div>
+    </article>
+<?php
+}
+if (isset($_POST["cc"]) && isset($_POST["vatnum"]) && isset($_POST["webid"])) {
+  $cc = (isset($_POST["cc"]) ? $_POST["cc"] : "NL");
+  $vatnum = (isset($_POST["vatnum"]) ? $_POST["vatnum"] : "862637223B01");
+  $webid = (isset($_POST["webid"]) ? $_POST["webid"] : "pondersource.com/id");
+
   $client = new SoapClient("http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl");
   $result = $client->checkVat(array('countryCode' => $_POST["cc"], 'vatNumber' => $_POST["vatnum"]));
   error_log(var_export($result, true));
 
   if ($result->valid) {
-    echo "<p>Your details:</p>";
-    echo "<p>Name: '$result->name'</p>";
-    echo "<p>Address: '$result->address'</p>";
-  
-    $certUrl = $server . $path;
+    $countries = getCountries();
+    $country = $countries[$cc];
+?>
+    <header>
+      <h2>Welcome to Connect your Books!</h2>
+    </header>
+    <article>
+<?php
+    echo "<p>Your request to join has been approved. ";
+    echo "Now please go to the settings in your bookkeeping system (e.g. the PeppolNext app in your Nextcloud server)";
+    echo "and set your AS4-to-Peppol gateway to:</p> <pre>http://fwd.connectyourbooks.com</pre>";
+
+    echo "<h2>Your details:</h2>";
+    echo "<p><strong>Name:</strong> $result->name</td></tr>";
+    echo "<p><strong>Address:</strong> $result->address</p>";
+    echo "<p><strong>Country:</strong> $country</p>";
+    echo "<p><strong>VAT Number:</strong> $vatnum</p>";
+    echo "<p><strong>WebID:</strong> $webid</p>";
+
     $client = new \GuzzleHttp\Client();
-    $response = $client->request('GET', $certUrl);
+    if (!str_starts_with($webid, "http")) {
+      $webid = "https://" . $webid;
+    }
+    $response = $client->request('GET', $webid);
   
     $statusCode = $response->getStatusCode();
     //echo $res->getHeader('content-type')[0];
     $responseBody = (string) $response->getBody();
     if($statusCode == 200) {
       if(strlen($responseBody) == 0) {
-        echo "Received empty response body from $server$path.";
+        echo "Received empty response body from $webid.";
       } else {
-        $cert = new X509;
-        $cert->loadX509($responseBody);
-        echo "<p>Found cert at $server$path</p>";
-        // echo "<p>$responseBody</p>";
-        echo "<pre>".$cert->getPublicKey()."</pre>";
+        // following https://stackoverflow.com/questions/31165989/parsing-turtle-rdf-from-string-to-array
+        $graph = new EasyRdf\Graph();
+        $graph->parse($responseBody,'turtle');
+        $array = $graph->toRdfPhp();
+        unset($graph);
+        $pubkey = $array['.']['http://federatedbookkeeping.org/ns/as4#pubkey'][0]['value'];
+        echo "<p><strong>AS4 pubkey parsed from your webid profile:</strong></p><pre>$pubkey</pre>";
       }
     } else {
-      echo "Attempt to retrieve cert from $server$path resulted in a $statusCode response code.";
+      echo "Attempt to retrieve cert from $webid resulted in a $statusCode response code.";
     }
-  
-    echo "<p>Welcome! Your request to join has been approved.</p>";
-    echo "<p>Now please go to the settings in your bookkeeping system (e.g. the PeppolNext app in your Nextcloud server)</p>";
-    echo "<p>and set your AS4-to-Peppol gateway to <tt>https://connectyourbooks.com</tt>.</p>";
   } else {
     echo "Not valid!";
   }
+} else {
+  displayForm();
 }
 ?>
-    <div>
   </body>
 </html>
