a6726ac893275b592618e2eceb5909c6667d59ee
Yashar PourMohammad <yasharpm@gmail.com>
2022-11-23 11:35:47 +0000

    add contacts app

--- docker/nextcloud/init.sh
+++ docker/nextcloud/init.sh
@@ -1,4 +1,5 @@
 php console.php maintenance:install --admin-user $USER --admin-pass $PASS --database "mysql" --database-name "nextcloud" --database-user "root" --database-pass "eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek" --database-host "$DBHOST"
+php console.php app:install contacts
 php console.php app:enable peppolnext
 php console.php app:enable groupfolders
 sed -i "8 i\      1 => 'nc1.docker'," /var/www/html/config/config.php
