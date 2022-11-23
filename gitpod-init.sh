d03c3f1221c5dd4dc75a609e47328536ad2c8aad
Yashar PourMohammad <yasharpm@gmail.com>
2022-11-23 11:20:03 +0000

    pull firefox image

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -5,6 +5,7 @@ set -e
 ./scripts/rebuild.sh
 ./scripts/transportp12.sh
 docker pull mariadb
+docker pull jlesage/firefox:v1.17.1
 
 export PEPPOL_PHP_DIR=`pwd`
 docker run -w /var/www/html/apps/peppolnext -v $PEPPOL_PHP_DIR:/var/www/html/apps/peppol-php nc2 make composer
